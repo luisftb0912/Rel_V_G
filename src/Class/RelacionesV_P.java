9fa56c3612e9509754bcf2b993b75ee084782b9d
luisftb <luisftb1@gmail.com>
2021-04-23 07:43:42 -0500

    archivos v2

---
+++ src/Class/RelacionesV_P.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Class;
+
+/**
+ *
+ * @author Name.inc
+ */
+public class RelacionesV_P {
+
+    //atributos Volumetricas
+    private int Relacion_vacios;
+    private int Porosidad;
+    private int grado_Saturacion;
+    //Atributos Gravimetricas
+    private double Humedad;
+    private int Peso_Espec;
+    private int Peso_Espec_Seco;
+    private int Grav_Espec_sol;
+    //
+
+    public int getRelacion_vacios() {
+        return Relacion_vacios;
+    }
+
+    public void setRelacion_vacios(int Vv, int Vs) {
+        int Resp_Vacios = Vv / Vs;
+        this.Relacion_vacios = Resp_Vacios;
+    }
+
+    public int getPorosidad() {
+        return Porosidad;
+    }
+
+    public void setPorosidad(int Vv, int Vt) {
+        int Resp_Porosidad = (Vv / Vt) * 100;
+        this.Porosidad = Resp_Porosidad;
+    }
+
+    public int getGrado_Saturacion() {
+        return grado_Saturacion;
+    }
+
+    public void setGrado_Saturacion(int Vw, int Vv) {
+        int Resp_Gs = (Vw / Vv) * 100;
+        this.grado_Saturacion = Resp_Gs;
+    }
+
+    public double getHumedad() {
+        return Humedad;
+    }
+
+    public void setHumedad(double Ww, double Ws) {
+        double Resp_Hum =  Math. round(((Ww / Ws) * 100)*100.0)/100.0;;
+        this.Humedad = Resp_Hum;
+    }
+
+    public int getPeso_Espec() {
+        return Peso_Espec;
+    }
+
+    public void setPeso_Espec(int Wt,int Vt) {
+         int Resp_PesEs = (Wt / Vt) * 100;
+        this.Peso_Espec = Resp_PesEs ;
+    }
+
+    public int getPeso_Espec_Seco() {
+        return Peso_Espec_Seco;
+    }
+
+    public void setPeso_Espec_Seco(int Ws,int Vt) {
+        int Resp_PesEsSec = (Ws / Vt) * 100;
+        this.Peso_Espec_Seco = Resp_PesEsSec;
+    }
+
+    public int getGrav_Espec_sol() {
+        return Grav_Espec_sol;
+    }
+
+    public void setGrav_Espec_sol(int Grav_Espec_sol) {
+        this.Grav_Espec_sol = Grav_Espec_sol;
+    }
+
+}
---
+++ src/Class/SueloMuestra.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Class;
+
+/**
+ *
+ * @author Name.inc
+ */
+public class SueloMuestra {
+    // atributos Volumen
+    private int Vol_Vacios;
+    private int Vol_Agua;
+    private int Vol_Solidos;
+    private int Vol_Aire;
+    private int Vol_Total;
+    //atributos Peso
+    private int Pes_Agua;
+    private int Pes_Solidos;
+    private int Pes_Aire;
+    private int Pes_Total;
+
+    //Getters Y Setters
+
+    public int getVol_Vacios() {
+        return Vol_Vacios;
+    }
+
+    public void setVol_Vacios(int Vol_Vacios) {
+        this.Vol_Vacios = Vol_Vacios;
+    }
+
+    public int getVol_Agua() {
+        return Vol_Agua;
+    }
+
+    public void setVol_Agua(int Vol_Agua) {
+        this.Vol_Agua = Vol_Agua;
+    }
+
+    public int getVol_Solidos() {
+        return Vol_Solidos;
+    }
+
+    public void setVol_Solidos(int Vol_Solidos) {
+        this.Vol_Solidos = Vol_Solidos;
+    }
+
+    public int getVol_Aire() {
+        return Vol_Aire;
+    }
+
+    public void setVol_Aire(int Vol_Aire) {
+        this.Vol_Aire = Vol_Aire;
+    }
+
+    public int getVol_Total() {
+        return Vol_Total;
+    }
+
+    public void setVol_Total(int Vol_Total) {
+        this.Vol_Total = Vol_Total;
+    }
+
+    public int getPes_Agua() {
+        return Pes_Agua;
+    }
+
+    public void setPes_Agua(int Pes_Agua) {
+        this.Pes_Agua = Pes_Agua;
+    }
+
+    public int getPes_Solidos() {
+        return Pes_Solidos;
+    }
+
+    public void setPes_Solidos(int Pes_Solidos) {
+        this.Pes_Solidos = Pes_Solidos;
+    }
+
+    public int getPes_Aire() {
+        return Pes_Aire;
+    }
+
+    public void setPes_Aire(int Pes_Aire) {
+        this.Pes_Aire = Pes_Aire;
+    }
+
+    public int getPes_Total() {
+        return Pes_Total;
+    }
+
+    public void setPes_Total(int Pes_Total) {
+        this.Pes_Total = Pes_Total;
+    }
+    
+    
+    
+}
